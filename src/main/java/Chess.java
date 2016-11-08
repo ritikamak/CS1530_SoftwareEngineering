15c216ab0c5503a0ea6d9a8bf20d1da2b7ada2ad
patelrohanv <patelrohanv@gmail.com>
2016-11-07 20:36:44 -0500

    delete everything
    deleted all the stockfish stuff, going to the merge the stuff with king
    and start over with stockfish

--- src/main/java/Chess.java
+++ src/main/java/Chess.java
@@ -6,11 +6,9 @@ public class Chess
 	public static Game game;
 	public static UserInput ui;
 	public static ChessGUI gui;
-	public static Stockfish engine;
 	public static void main(String[] args)
 	{
 		game = new Game();
-		engine = new Stockfish();
 		game.setup();
 		ui = new UserInput(game);
 		gui = new ChessGUI(game, ui);
